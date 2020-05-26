e08514d08595aff4bc6194f8b5f73f77688313f5
Thomas Zdyrski <tzdyrski@ucsd.edu>
2020-05-26 16:27:52 -0700

    Decrease zoom on Jeffreys Power Spectrum plot.

--- Code/IntermediateNumericalSolver.py
+++ Code/IntermediateNumericalSolver.py
@@ -2012,7 +2012,7 @@ if(plot_power_spec_Jeffreys):
     ax[1].plot(kappa,negSnapshotsPower)
 
     # Put insets to zoom-in around second harmonic
-    axins = [zoomed_inset_axes(ax[0], zoom=10, loc=1),
+    axins = [zoomed_inset_axes(ax[0], zoom=2.5, loc=1),
              zoomed_inset_axes(ax[1], zoom=10, loc=1)]
 
     axins[0].plot(kappa,posSnapshotsPower)
