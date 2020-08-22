405451db3db174ed29f5fb2336e34fc143ecb71c
Thomas Zdyrski <tzdyrski@ucsd.edu>
2020-08-21 23:13:26 -0700

    Adjust figure height to shrink manuscript size.

--- Code/plotter.py
+++ Code/plotter.py
@@ -192,8 +192,7 @@ def plot_multiplot_template(data_arrays, x_coordinate,
 
     # Adjust figure height
     figsize = fig.get_size_inches()
-    fig.set_size_inches([figsize[0],figsize[1]*(0.8+0.2*ax.shape[0])])
-
+    fig.set_size_inches([figsize[0],figsize[1]*(0.6+0.2*ax.shape[0])])
 
     # Save parameters into a separate array of dictionaries so we can
     # edit without affecting the dictionary
