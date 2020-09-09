Fix missing factor of epsilon in tail plot.
It is unclear why this is necessary, but it makes the approximation fit
better.

Code/plotter.py
@@ -1344,7 +1344,7 @@ def plot_pos_neg_solitary_tail(load_prefix, save_prefix, *args, **kwargs):
         symmetric_approx = data_array.attrs['eps']*\
                 H/np.cosh(np.sqrt(H/8)*\
                 (data_array.coords['x/h']*np.sqrt(data_array.attrs['mu'])-\
-                (H-2)/2*data_array.coords['t*eps*sqrt(g*h)*k_E'])
+                (H/2-1)*data_array.attrs['eps']*data_array.coords['t*eps*sqrt(g*h)*k_E'])
                 )**2
 
         data_arrays[indx] = data_array - symmetric_approx