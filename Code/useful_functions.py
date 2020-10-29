ef9898d1e3706fe1ba704dfc9b601ba2f913e0ee
Thomas Zdyrski <tzdyrski@ucsd.edu>
2020-10-29 09:54:16 -0700

    Add accuracy option to finite difference derivative.

--- Code/useful_functions.py
+++ Code/useful_functions.py
@@ -23,6 +23,7 @@ def round_sig_figs(number, num_sig_figs=3):
     return result
 
 def derivative(u, dx=1, period=2*np.pi, axis=0, order=1,
+        accuracy=4,
         deriv_type='gradient', *args, **kwargs):
     """Calculate the derivative of order 'order'.
 
@@ -38,6 +39,8 @@ def derivative(u, dx=1, period=2*np.pi, axis=0, order=1,
         Axis along which to take the derivative. Default is 0.
     order : integer
         Order of derivative. Default is 1.
+    accuracy : {2,4}
+        Order of accuracy for 'periodic_fd'. Default is 4.
     deriv_type : 'gradient', 'FFT', or 'periodic_fd'
         The type of derivative to take. 'gradient' yields a
         non-periodic, finite-difference derivative. 'FFT' yields a
@@ -56,76 +59,139 @@ def derivative(u, dx=1, period=2*np.pi, axis=0, order=1,
         # Compute the x derivatives using the pseudo-spectral method
         derivative = psdiff(u, period=period, order=order)
     elif deriv_type == 'periodic_fd':
-        # 4th-order center difference with periodic boundary conditions
-        if order == 1:
-            u_padded_twice = np.concatenate((u[-2:], u, u[0:2]))
-            derivative = (
-                    - 1/12*u_padded_twice[4:]
-                    + 2/3*u_padded_twice[3:-1]
-                    - 2/3*u_padded_twice[1:-3]
-                    + 1/12*u_padded_twice[0:-4]
-                    )/\
-                    (dx)
-        elif order == 2:
-            u_padded_twice = np.concatenate((u[-2:], u, u[0:2]))
-            derivative = (
-                    - 1/12*u_padded_twice[4:]
-                    + 4/3*u_padded_twice[3:-1]
-                    - 5/2*u_padded_twice[2:-2]
-                    + 4/3*u_padded_twice[1:-3]
-                    - 1/12*u_padded_twice[0:-4]
-                    )/\
-                    (dx**2)
-        elif order == 3:
-            u_padded_thrice = np.concatenate((u[-3:], u, u[0:3]))
-            derivative = (
-                    - 1/8*u_padded_thrice[6:]
-                    + 1*u_padded_thrice[5:-1]
-                    - 13/8*u_padded_thrice[4:-2]
-                    + 13/8*u_padded_thrice[2:-4]
-                    - 1*u_padded_thrice[1:-5]
-                    + 1/8*u_padded_thrice[0:-6]
-                    )/(dx**3)
-        elif order == 4:
-            u_padded_thrice = np.concatenate((u[-3:], u, u[0:3]))
-            derivative = (
-                    - 1/6*u_padded_thrice[6:]
-                    + 2*u_padded_thrice[5:-1]
-                    - 13/2*u_padded_thrice[4:-2]
-                    + 28/3*u_padded_thrice[3:-3]
-                    - 13/2*u_padded_thrice[2:-4]
-                    + 2*u_padded_thrice[1:-5]
-                    - 1/6*u_padded_thrice[0:-6]
-                    )/(dx**4)
-        elif order == 5:
-            u_padded_quatrice = np.concatenate((u[-4:], u, u[0:4]))
-            derivative = (
-                    - 1/6*u_padded_quatrice[8:]
-                    + 3/2*u_padded_quatrice[7:-1]
-                    - 13/3*u_padded_quatrice[6:-2]
-                    + 29/6*u_padded_quatrice[5:-3]
-                    - 29/6*u_padded_quatrice[3:-5]
-                    + 13/3*u_padded_quatrice[2:-6]
-                    - 3/2*u_padded_quatrice[1:-7]
-                    + 1/6*u_padded_quatrice[0:-8]
-                    )/(dx**5)
-        elif order == 6:
-            u_padded_quatrice = np.concatenate((u[-4:], u, u[0:4]))
-            derivative = (
-                    - 1/4*u_padded_quatrice[8:]
-                    + 3*u_padded_quatrice[7:-1]
-                    - 13*u_padded_quatrice[6:-2]
-                    + 29*u_padded_quatrice[5:-3]
-                    - 75/2*u_padded_quatrice[4:-4]
-                    + 29*u_padded_quatrice[3:-5]
-                    - 13*u_padded_quatrice[2:-6]
-                    + 3*u_padded_quatrice[1:-7]
-                    - 1/4*u_padded_quatrice[0:-8]
-                    )/(dx**6)
+        if accuracy == 2:
+            # 2nd-order center difference with periodic boundary conditions
+            if order == 1:
+                u_padded_once = np.concatenate((u[[-1]], u, u[[0]]))
+                derivative = (
+                        u_padded_once[2:]
+                        -u_padded_once[0:-2]
+                        )/\
+                        (2*dx)
+            elif order == 2:
+                u_padded_once = np.concatenate((u[[-1]], u, u[[0]]))
+                derivative = (
+                        u_padded_once[2:]
+                        - 2*u_padded_once[1:-1]
+                        + u_padded_once[0:-2]
+                        )/(dx**2)
+            elif order == 3:
+                u_padded_twice = np.concatenate((u[-2:], u, u[0:2]))
+                derivative = (
+                        u_padded_twice[4:]
+                        - 2*u_padded_twice[3:-1]
+                        + 2*u_padded_twice[1:-3]
+                        - u_padded_twice[0:-4]
+                        )/(2*dx**3)
+            elif order == 4:
+                u_padded_twice = np.concatenate((u[-2:], u, u[0:2]))
+                derivative = (
+                        u_padded_twice[4:]
+                        - 4*u_padded_twice[3:-1]
+                        + 6*u_padded_twice[2:-2]
+                        - 4*u_padded_twice[1:-3]
+                        + u_padded_twice[0:-4]
+                        )/(dx**4)
+            elif order == 5:
+                u_padded_thrice = np.concatenate((u[-3:], u, u[0:3]))
+                derivative = (
+                        u_padded_thrice[6:]
+                        - 4*u_padded_thrice[5:-1]
+                        + 5*u_padded_thrice[4:-2]
+                        - 5*u_padded_thrice[2:-4]
+                        + 4*u_padded_thrice[1:-5]
+                        - u_padded_thrice[0:-6]
+                        )/(2*dx**5)
+            elif order == 6:
+                u_padded_thrice = np.concatenate((u[-3:], u, u[0:3]))
+                derivative = (
+                        + 1*u_padded_thrice[6:]
+                        - 6*u_padded_thrice[5:-1]
+                        + 15*u_padded_thrice[4:-2]
+                        - 20*u_padded_thrice[3:-3]
+                        + 15*u_padded_thrice[2:-4]
+                        - 6*u_padded_thrice[1:-5]
+                        + 1*u_padded_thrice[0:-6]
+                        )/(dx**6)
+            else:
+                raise(ValueError("Derivatives of type 'periodic_fd'"+\
+                        "are only supported up to order 6, but "+\
+                        str(order)+" was given"))
+        elif accuracy == 4:
+            # 4th-order center difference with periodic boundary conditions
+            if order == 1:
+                u_padded_twice = np.concatenate((u[-2:], u, u[0:2]))
+                derivative = (
+                        - 1/12*u_padded_twice[4:]
+                        + 2/3*u_padded_twice[3:-1]
+                        - 2/3*u_padded_twice[1:-3]
+                        + 1/12*u_padded_twice[0:-4]
+                        )/\
+                        (dx)
+            elif order == 2:
+                u_padded_twice = np.concatenate((u[-2:], u, u[0:2]))
+                derivative = (
+                        - 1/12*u_padded_twice[4:]
+                        + 4/3*u_padded_twice[3:-1]
+                        - 5/2*u_padded_twice[2:-2]
+                        + 4/3*u_padded_twice[1:-3]
+                        - 1/12*u_padded_twice[0:-4]
+                        )/\
+                        (dx**2)
+            elif order == 3:
+                u_padded_thrice = np.concatenate((u[-3:], u, u[0:3]))
+                derivative = (
+                        - 1/8*u_padded_thrice[6:]
+                        + 1*u_padded_thrice[5:-1]
+                        - 13/8*u_padded_thrice[4:-2]
+                        + 13/8*u_padded_thrice[2:-4]
+                        - 1*u_padded_thrice[1:-5]
+                        + 1/8*u_padded_thrice[0:-6]
+                        )/(dx**3)
+            elif order == 4:
+                u_padded_thrice = np.concatenate((u[-3:], u, u[0:3]))
+                derivative = (
+                        - 1/6*u_padded_thrice[6:]
+                        + 2*u_padded_thrice[5:-1]
+                        - 13/2*u_padded_thrice[4:-2]
+                        + 28/3*u_padded_thrice[3:-3]
+                        - 13/2*u_padded_thrice[2:-4]
+                        + 2*u_padded_thrice[1:-5]
+                        - 1/6*u_padded_thrice[0:-6]
+                        )/(dx**4)
+            elif order == 5:
+                u_padded_quatrice = np.concatenate((u[-4:], u, u[0:4]))
+                derivative = (
+                        - 1/6*u_padded_quatrice[8:]
+                        + 3/2*u_padded_quatrice[7:-1]
+                        - 13/3*u_padded_quatrice[6:-2]
+                        + 29/6*u_padded_quatrice[5:-3]
+                        - 29/6*u_padded_quatrice[3:-5]
+                        + 13/3*u_padded_quatrice[2:-6]
+                        - 3/2*u_padded_quatrice[1:-7]
+                        + 1/6*u_padded_quatrice[0:-8]
+                        )/(dx**5)
+            elif order == 6:
+                u_padded_quatrice = np.concatenate((u[-4:], u, u[0:4]))
+                derivative = (
+                        - 1/4*u_padded_quatrice[8:]
+                        + 3*u_padded_quatrice[7:-1]
+                        - 13*u_padded_quatrice[6:-2]
+                        + 29*u_padded_quatrice[5:-3]
+                        - 75/2*u_padded_quatrice[4:-4]
+                        + 29*u_padded_quatrice[3:-5]
+                        - 13*u_padded_quatrice[2:-6]
+                        + 3*u_padded_quatrice[1:-7]
+                        - 1/4*u_padded_quatrice[0:-8]
+                        )/(dx**6)
+            else:
+                raise(ValueError("Derivatives of type 'periodic_fd'"+\
+                        "are only supported up to order 6, but "+\
+                        str(order)+" was given"))
         else:
             raise(ValueError("Derivatives of type 'periodic_fd'"+\
-                    "are only supported up to order 6, but "+\
-                    str(order)+" was given"))
+                    "are only supported for accuracies 2 and 4, but "+\
+                    str(accuracy)+" was given"))
     else:
         raise(ValueError("'deriv_type' must be either 'gradient',"+\
         "'FFT', or 'periodic_fd', but "+deriv_type+" was given"))
